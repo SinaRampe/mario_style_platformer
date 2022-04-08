654fade59fcdec8bc065bdca4a78315a372691bb
Sina <sinarampe@gmail.com>
2022-04-08 15:38:57 +0200

    implemented first player graphic

--- code/player.py
+++ code/player.py
@@ -5,8 +5,9 @@ class Player(pygame.sprite.Sprite):
     def __init__(self, pos):
         super().__init__()
         self.import_character_assets()
-        self.image = pygame.Surface((32, 64))
-        self.image.fill("red")
+        self.frame_index = 0
+        self.animation_speed = 0.15
+        self.image = self.animations["idle"][self.frame_index]
         self.rect = self.image.get_rect(topleft = pos)
 
         #player movement
@@ -16,12 +17,14 @@ class Player(pygame.sprite.Sprite):
         self.jump_speed = -16
 
     def import_character_assets(self):
-        character_path = "../graphics/character/"
+        character_path = "graphics/character/"
         self.animations = {"idle": [], "run": [], "jump": [], "fall": []}
 
-        for animation in self.animations:
+        for animation in self.animations.keys():
             full_path = character_path + animation 
             self.animations[animation] = import_folder(full_path)
+
+
     def get_input(self):
         keys = pygame.key.get_pressed()
 
--- code/support.py
+++ code/support.py
@@ -2,11 +2,16 @@ from os import walk
 import pygame
 
 def import_folder(path):
-    surface_list = []
+	surface_list = []
 
-    for _, __, img_files in walk(path):
-        for img_file in img_files:
-            img_surf = pygame.image.load(f"graphics/player/{img_file}").convert_alpha()
-            surface_list.append(img_surf)
+	for _,__,image_files in walk(path):
+		for image in image_files:
+            
+			full_path = path + "/" + image
+			image_surf = pygame.image.load(full_path).convert_alpha()
+			surface_list.append(image_surf)
+            
 
-    return surface_list
+	return surface_list 
+
+

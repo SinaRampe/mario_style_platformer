ef0ce9922f6057d6c01221cbb9b3542c28feabdd
Sina <sinarampe@gmail.com>
2022-04-09 09:07:32 +0200

    animation left right implemented

--- code/player.py
+++ code/player.py
@@ -18,6 +18,7 @@ class Player(pygame.sprite.Sprite):
 
         #player status
         self.status = "idle"
+        self.facing_right = True
 
     def import_character_assets(self):
         character_path = "graphics/character/"
@@ -34,15 +35,23 @@ class Player(pygame.sprite.Sprite):
         if self.frame_index >= len(animation):
             self.frame_index = 0
 
-        self.image = animation[int(self.frame_index)]
+        image = animation[int(self.frame_index)]
+        if self.facing_right:
+            self.image = image
+        else:
+            flipped_image = pygame.transform.flip(image, True, False)
+            self.image = flipped_image
+
 
     def get_input(self):
         keys = pygame.key.get_pressed()
 
         if keys[pygame.K_RIGHT]:
             self.direction.x = 1
+            self.facing_right = True
         elif keys[pygame.K_LEFT]:
             self.direction.x = -1
+            self.facing_right = False
         else:
             self.direction.x = 0
 
@@ -52,7 +61,7 @@ class Player(pygame.sprite.Sprite):
     def get_status(self):
         if self.direction.y < 0:
             self.status = "jump"
-        elif self.direction.y > 0:
+        elif self.direction.y > 1:
             self.status = "fall"
         else:
             if self.direction == 0:
